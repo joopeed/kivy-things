e113c237c2adddc1334773862e9a7a7754f7c7dc
João Pedro Leôncio <joopeeds@gmail.com>
2014-04-16 00:13:44 -0300

    Mais

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ListProperty
+from kivy.graphics import *
 
 class Peca(Widget):
-    pass
+    def __init__(self, x, y, **kwargs):
+        super(Peca, self).__init__(**kwargs)
+        self.bind(pos=self.update_canvas)
+        self.bind(size=self.update_canvas)
+        self.update_canvas()    
+        
+    def update_canvas(self, *args):
+        self.canvas.clear()
+        Ellipse(pos = self.pos, size = self.size)
 
 class Tabuleiro(Widget):
     def __init__(self, **kwargs):
         super(Tabuleiro, self).__init__(**kwargs)
-        self.layout = GridLayout(cols=2)
-        self.layout.add_widget(Peca())
-        self.table = ListProperty([[Peca(), Peca()],[Peca(), Peca()]])
+        self.table = [[1, 1],[1, 1]]
+        with self.canvas:
+            for i in range(len(self.table)):
+                for j in range(len(self.table[0])):
+                    self.table[i][j] = Peca(i*100, j*100)  
     
 
 class PongApp(App):
